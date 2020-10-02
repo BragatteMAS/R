8ff7e7477d17149dc4756cb25d94d0ec9f0f3e89
Bragatte <marcelobragatte@gmail.com>
2020-10-02 00:10:07 -0300

    Add files via upload

---
+++ UpdatesR.R
@@ -0,0 +1,13 @@
+# Updates for RStudio
+install.packages("esquisse")
+install.packages("questionr")
+install.packages("remedy")
+install.packages("styler")
+install.packages("snakecaser")
+
+install.packages("ymlthis")
+install.packages("blogdown")
+
+install.packages("viewpipesteps")
+devtools::install_github("daranzolin/ViewPipeSteps")
+library(ViewPipeSteps)

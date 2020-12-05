713c778b5247f1a707445dc743c32d1bd491b3ff
Bragatte <marcelobragatte@gmail.com>
2020-12-05 18:45:04 -0300

    Create PNAD.R

---
+++ PNAD.R
@@ -0,0 +1,9 @@
+#Bragatte PNAD stats
+
+#install
+packs=c("tidyr", "geobr", "survey") 
+lapply(packs, install.packages, character.only=TRUE)
+
+#libraries
+lapply(packs, require, character.only=TRUE)
+

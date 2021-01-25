46d952c6e8d3cebc30d7924efb521d7618f17a39
Bragatte <marcelobragatte@gmail.com>
2021-01-24 23:52:13 -0300

    Update 3_How_to_torch.R

--- 3_How_to_torch.R
+++ 3_How_to_torch.R
@@ -15,9 +15,32 @@ plot(f)
 
 #Aim: Find X who reduce F
 #Tensor == matriz (multi)
-#gradiente_descend
+#Torch do Autograd
 #x, df/dx
 #where X & cost derivate from X
-x <-  torch_tensor(0, requires_grad = TRUE)
+x <- torch_tensor(0, requires_grad = TRUE)
 
 x$grad
+#gradient_descend
+#x[t+1] = x[t] - a * df/dx[t]
+
+optim <- optim_adam(x) #improve formulas
+
+fx <- f(x) #calc f(x)
+fx$backward() #backpropagation - f direction calc derivative
+x$grad
+
+optim$step()
+x
+
+points(x, fx, col = "red")
+
+for (pass in 2:100) {
+    optim$zero_grad()
+    
+    fx <- f(x)
+    fx$backward()
+    optim$step()
+    points(x, fx, col = "red")
+    Sys.sleep(0.1)
+}

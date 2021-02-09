c76a143835a110690d252bb043b074c06139b135
Bragatte <marcelobragatte@gmail.com>
2021-02-09 17:06:14 -0300

    Update 4_How_to_Chi_Square.R

--- 4_How_to_Chi_Square.R
+++ 4_How_to_Chi_Square.R
@@ -1,9 +1,15 @@
 #How_to_ChiSquare - Disfuncao_Eretil created by Bragatte  202102062017
 #chamar arquivo
-disf_eret<- read_delim("Downloads/bd_pre_pos.csv", ";")
-disf_eret_nopac<- read_delim("Downloads/bd_pre_pos2.csv", ";")
+disf_eret <-
+    read.csv("~/Downloads/bd_pre_pos.csv",
+             sep = ",",
+             header = T)
+disf_eret_nopac <-
+    read.csv("~/Downloads/bd_pre_pos_nopac.csv",
+             sep = ",",
+             header = T)
 
-View(disf_eret)
+View(disf_eret_nopac)
 
 #Criar variável para comparar duas colunas de interesse
 x2 = table(disf_eret$Grupo, disf_eret$IIEF_grav_2)
@@ -13,8 +19,13 @@ x2
 chisq.test(x2)
 
 #valor p direto do qui-quadraro
-chisq.test(x2)$p.value 
+chisq.test(x2)$p.value
 
 ##segundo método resumo dos dados
 summary(x2)
 
+###
+
+library(corrplot)
+correlation = disf_eret_nopac.corr()
+corrplot(corr, method ='number')

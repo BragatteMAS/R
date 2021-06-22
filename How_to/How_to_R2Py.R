ad2b5087a0badac5fdec582afbbea24cb5a0b195
Bragatte <marcelobragatte@gmail.com>
2021-06-21 23:59:21 -0300

    Update How_to_R2Py.R

--- How_to/How_to_R2Py.R
+++ How_to/How_to_R2Py.R
@@ -2,6 +2,7 @@
 "R2Py"
 created by "Bragatte" 20210211
 '''
+
 #seeting env py at the first time and restart R
 ##add this to open file: Sys.setenv(RETICULATE_PYTHON = "/home/bragatte/anaconda3/envs/dsz/bin/python")
 '''
@@ -10,7 +11,7 @@ usethis::edit_r_profile()
 #check py version
 reticulate::py_config()
 #libraries
-packs = c("reticulate", "png")
+packs = c("reticulate", "png", "yamlthis")
 lapply(packs, install.packages, character.only = TRUE)
 lapply(packs, require, character.only = TRUE)
 

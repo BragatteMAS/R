9fb846bdaff595a8b72c5b8e49f8061fd8d25c3b
Bragatte <marcelobragatte@gmail.com>
2022-11-07 20:05:42 -0300

    data correction

---
+++ LeaRn/correct data/correct_data_format.R
@@ -0,0 +1,15 @@
+# Correct data format
+#R 4.2.1
+#@BragatteMAS
+#########################################
+# install
+packs = c("vroom","lubridate" )
+#lapply(packs, install.packages, character.only = TRUE)
+
+# libraries
+lapply(packs, require, character.only = TRUE)
+
+## sifilis file
+sif <- vroom("sisab_pe_cid_sifilis.csv")
+#add day 01 and change format
+sif$mes <- lubridate::as_date(paste("01", sif$mes), format = "%d_%b_%y")

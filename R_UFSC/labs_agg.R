f4557ba856958441c4015dc3ec926c36dec58f49
Bragatte <marcelobragatte@gmail.com>
2023-07-12 09:16:29 -0300

    Create labs_agg.R

---
+++ R_UFSC/labs_agg.R
@@ -0,0 +1,39 @@
+#########################################
+## chama bibliotecas
+pacman::p_load(tidyverse, vroom)
+
+## chama dados Lab
+hilab <- vroom("~/Downloads/Exames da Semana 04-07-23.csv")
+## Filtrar o dataframe com base nas condições
+hilab_uniq <- unique(hilab$Exame[hilab$Exame == "Covid-19 Antígeno"], hilab$`Código Da Cápsula`)
+
+## select Covid
+df_hi <- hilab |> 
+  filter(hilab$Exame == 'Covid-19 Antígeno')
+## select Flu A
+df_hi_flu <- hilab |> 
+  filter(hilab$Exame == 'Influenza A')
+## select FLu B
+df_hi_flu <- hilab |> 
+  filter(hilab$Exame == 'Influenza B')
+
+# Selecionar as colunas desejadas
+sel <- hilab[, c("Código Da Cápsula", "Exame", "Paciente")]
+
+sel_cov <- df_hi[, c("Código Da Cápsula", "Exame", "Paciente")]
+
+sel_fluA <- df_hi_flu[, c("Código Da Cápsula", "Exame", "Paciente")]
+
+sel_fluB <- df_hi_flu[, c("Código Da Cápsula", "Exame", "Paciente")]
+
+
+# Obter os valores únicos das colunas selecionadas
+valores_unicos <- unique(sel)
+
+val_uniq_cov <- unique(sel_cov)
+
+val_uniq_flua <- unique(sel_fluA)
+
+val_uniq_fluB <- unique(sel_fluB)
+
+#########################################

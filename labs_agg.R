12f159aa5b10cd0596b540c8be0281720c0a4a78
Bragatte <marcelobragatte@gmail.com>
2023-10-04 23:03:42 -0300

    Update labs_agg.R

--- labs_agg.R
+++ labs_agg.R
@@ -2,10 +2,10 @@
 ## chama bibliotecas
 pacman::p_load(tidyverse, vroom)
 
-## 24_resp
+## 25_resp
 combined <-
   # vroom("~/Documents/23_respat/results/combined.tsv")
-  vroom("~/Documents/20230927_relatorio24_resp/respat/results/combined.tsv")
+  vroom("~/Documents/SE39/respat/results/combined.tsv")
 
 ## test_result by labs
 table(combined$SC2_test_result, combined$lab_id)
@@ -17,8 +17,11 @@ table(combined$test_kit, combined$lab_id)
 table(combined$state, combined$SC2_test_result)
 
 ## test_lab by last week
-last_week <- combined[combined$epiweek == "2023-09-23", ]
+last_week <- combined[combined$epiweek == "2023-09-30", ]
 table(last_week$lab_id, last_week$epiweek)
+
+## test_kit by last week 
+table(last_week$test_kit, last_week$epiweek)
 
 ## Pos Neg by last week
 table(last_week$lab_id, last_week$SC2_test_result)
